6e9092ba31b6b527752d06a34f43397b10076c97
takokun <takokun.778@gmail.com>
2023-02-11 22:34:18 +0900

    chore: fix charts

--- okteto/sops.sh
+++ okteto/sops.sh
@@ -13,12 +13,12 @@ mv sops-v${SOPS_VERSION}.linux.amd64 sops
 chmod +x sops
 
 # シークレットの復号化
-./sops --decrypt --in-place charts/secret/api-key.yaml
+./sops --decrypt --in-place charts/secret/templates/api-key.yaml
 
-./sops --decrypt --in-place charts/secret/database-url.yaml
+./sops --decrypt --in-place charts/secret/templates/database-url.yaml
 
-./sops --decrypt --in-place charts/secret/redis-url.yaml
+./sops --decrypt --in-place charts/secret/templates/redis-url.yaml
 
-./sops --decrypt --in-place charts/secret/firebase-api-key.yaml
+./sops --decrypt --in-place charts/secret/templates/firebase-api-key.yaml
 
-./sops --decrypt --in-place charts/secret/firebase-secret.yaml
+./sops --decrypt --in-place charts/secret/templates/firebase-secret.yaml
