3272a58deb2893971eb656306d6a591aff8b4a79
takokun <takokun.778@gmail.com>
2023-02-11 23:09:40 +0900

    fix: app

--- okteto/prod.sh
+++ okteto/prod.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -eu
 
-helm upgrade --install --wait morning-night-dream-platform ./charts/app-core --namespace=toy-morning-night-dreamer
+helm upgrade --install --wait morning-night-dream-platform-app-core ./charts/app-core --namespace=toy-morning-night-dreamer
 
-helm upgrade --install --wait morning-night-dream-platform ./charts/app-gateway --namespace=toy-morning-night-dreamer
+helm upgrade --install --wait morning-night-dream-platform-app-gateway ./charts/app-gateway --namespace=toy-morning-night-dreamer
