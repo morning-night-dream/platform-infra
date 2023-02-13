0f43f0a3dfcda4f77fcb6ce9cb4da6bceaff6e4b
takokun <takokun.778@gmail.com>
2023-02-14 00:22:44 +0900

    fix: secret

--- okteto/prod.sh
+++ okteto/prod.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -eu
 
+cp charts/secret/env/prod/app-core/*.yaml charts/app-core/templates/secret/
+
 helm upgrade --install --wait morning-night-dream-platform-app-core ./charts/app-core --namespace=toy-morning-night-dreamer
+
+# cp charts/secret/env/prod/app-gateway/*.yaml charts/app-gateway/templates/secret/
 
 helm upgrade --install --wait morning-night-dream-platform-app-gateway ./charts/app-gateway --namespace=toy-morning-night-dreamer
