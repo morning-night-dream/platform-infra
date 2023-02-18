8bb441b7ac0f24a25469d147912d0b884923d3a8
takokun <takokun.778@gmail.com>
2023-02-18 15:15:10 +0900

    chore: gateway to api

--- okteto/prod.sh
+++ okteto/prod.sh
@@ -4,6 +4,6 @@ cp secret/env/prod/app-core/*.yaml charts/app-core/templates/secret/
 
 helm upgrade --install --wait morning-night-dream-platform-app-core ./charts/app-core --namespace=toy-morning-night-dreamer
 
-# cp secret/env/prod/app-gateway/*.yaml charts/app-gateway/templates/secret/
+# cp secret/env/prod/app-api/*.yaml charts/app-api/templates/secret/
 
-helm upgrade --install --wait morning-night-dream-platform-app-gateway ./charts/app-gateway --namespace=toy-morning-night-dreamer
+helm upgrade --install --wait morning-night-dream-platform-app-api ./charts/app-api --namespace=toy-morning-night-dreamer
