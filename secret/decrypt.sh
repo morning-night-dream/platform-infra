f7c8ab04bf5a19dbc2c4e1ff37123102de7a064e
takokun <takokun.778@gmail.com>
2023-03-13 00:12:03 +0900

    fix

--- secret/decrypt.sh
+++ secret/decrypt.sh
@@ -6,6 +6,8 @@ secret=$2
 
 sops --decrypt secret/env/${env}/${secret}.yaml > secret/env/${env}/${secret}.tmp.yaml
 
+touch secret/env/${env}/${secret}.in.txt
+
 yq ".data.${secret}" secret/env/${env}/${secret}.tmp.yaml | base64 -D > secret/env/${env}/${secret}.out.txt
 
 rm secret/env/${env}/${secret}.tmp.yaml
