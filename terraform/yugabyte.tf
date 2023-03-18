05d04ff0258f5e58b786b6beed51c8d582193864
takokun <takokun.778@gmail.com>
2023-03-18 19:12:54 +0900

    fix

--- terraform/yugabyte.tf
+++ terraform/yugabyte.tf
@@ -26,7 +26,7 @@ resource "ybm_cluster" "single_region_cluster" {
       num_nodes = 1
     }
   ]
-  cluster_tier           = "FREE"
+  cluster_tier = "FREE"
   node_config = {
     num_cores = 2
   }
