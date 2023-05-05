a85b20f9f6864008d723354171ccf09b5b6e9358
takokun <takokun.778@gmail.com>
2023-05-05 15:36:56 +0900

    fix

--- .okteto/okteto.sh
+++ .okteto/okteto.sh
@@ -1,1 +1,7 @@
 #!/bin/sh -eu
+
+helm upgrade --install --wait morning-night-dream-platform-database ./charts/database --namespace=toy-morning-night-dreamer
+
+helm upgrade --install --wait morning-night-dream-platform-app-core ./charts/app-core --namespace=toy-morning-night-dreamer
+
+helm upgrade --install --wait morning-night-dream-platform-app-api ./charts/app-api --namespace=toy-morning-night-dreamer
